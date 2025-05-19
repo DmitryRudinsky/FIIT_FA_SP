7d453acdda967ea9c1698450508f4e1d9c857817
DmitryRudinsky <guladmitriy1331@gmail.com>
2025-05-20 00:44:14 +0300

    buddies

--- allocator/allocator_buddies_system/include/allocator_buddies_system.h
+++ allocator/allocator_buddies_system/include/allocator_buddies_system.h
@@ -1,26 +1,23 @@
 #ifndef MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_BUDDIES_SYSTEM_H
 #define MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_BUDDIES_SYSTEM_H
 
-#include <pp_allocator.h>
 #include <allocator_test_utils.h>
 #include <allocator_with_fit_mode.h>
 #include <logger_guardant.h>
+#include <pp_allocator.h>
 #include <typename_holder.h>
-#include <mutex>
 #include <cmath>
+#include <cstddef>
+#include <mutex>
 
-namespace __detail
-{
-    constexpr size_t nearest_greater_k_of_2(size_t size) noexcept
-    {
+namespace __detail {
+    constexpr size_t nearest_greater_k_of_2(size_t size) noexcept {
         int ones_counter = 0, index = -1;
 
         constexpr const size_t o = 1;
 
-        for (int i = sizeof(size_t) * 8 - 1; i >= 0; --i)
-        {
-            if (size & (o << i))
-            {
+        for (int i = sizeof(size_t) * 8 - 1; i >= 0; --i) {
+            if (size & (o << i)) {
                 if (ones_counter == 0)
                     index = i;
                 ++ones_counter;
@@ -29,21 +26,16 @@ namespace __detail
 
         return ones_counter <= 1 ? index : index + 1;
     }
-}
+}// namespace __detail
 
-class allocator_buddies_system final:
-    public smart_mem_resource,
-    public allocator_test_utils,
-    public allocator_with_fit_mode,
-    private logger_guardant,
-    private typename_holder
-{
+class allocator_buddies_system final : public smart_mem_resource,
+                                       public allocator_test_utils,
+                                       public allocator_with_fit_mode,
+                                       private logger_guardant,
+                                       private typename_holder {
 
 private:
-
-
-    struct block_metadata
-    {
+    struct block_metadata {
         bool occupied : 1;
         unsigned char size : 7;
     };
@@ -54,15 +46,17 @@ private:
      * TODO: You must improve it for alignment support
      */
 
-    static constexpr const size_t allocator_metadata_size = sizeof(logger*) + sizeof(allocator_dbg_helper*) + sizeof(fit_mode) + sizeof(unsigned char) + sizeof(std::mutex);
+    static constexpr const size_t allocator_metadata_size = sizeof(logger *) + sizeof(allocator_dbg_helper *) + sizeof(fit_mode) + sizeof(unsigned char) + sizeof(std::mutex);
 
-    static constexpr const size_t occupied_block_metadata_size = sizeof(block_metadata) + sizeof(void*);
+    static constexpr const size_t occupied_block_metadata_size = sizeof(block_metadata) + sizeof(void *);
 
     static constexpr const size_t free_block_metadata_size = sizeof(block_metadata);
 
     static constexpr const size_t min_k = __detail::nearest_greater_k_of_2(occupied_block_metadata_size);
 
 public:
+    std::mutex &get_mutex() const;
+    allocator_with_fit_mode::fit_mode &get_fit_mode() const noexcept;
 
     explicit allocator_buddies_system(
             size_t space_size_power_of_two,
@@ -71,63 +65,58 @@ public:
             allocator_with_fit_mode::fit_mode allocate_fit_mode = allocator_with_fit_mode::fit_mode::first_fit);
 
     allocator_buddies_system(
-        allocator_buddies_system const &other);
-    
+            allocator_buddies_system const &other);
+
     allocator_buddies_system &operator=(
-        allocator_buddies_system const &other);
-    
+            allocator_buddies_system const &other);
+
     allocator_buddies_system(
-        allocator_buddies_system &&other) noexcept;
-    
+            allocator_buddies_system &&other) noexcept;
+
     allocator_buddies_system &operator=(
-        allocator_buddies_system &&other) noexcept;
+            allocator_buddies_system &&other) noexcept;
 
     ~allocator_buddies_system() override;
 
 public:
-    
-    [[nodiscard]] void *do_allocate_sm(
-        size_t size) override;
-    
-    void do_deallocate_sm(
-        void *at) override;
+    [[nodiscard]] void *do_allocate_sm(size_t size) override;
 
-    bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override;
+    void do_deallocate_sm(void *at) override;
 
-    inline void set_fit_mode(
-        allocator_with_fit_mode::fit_mode mode) override;
+    bool do_is_equal(const std::pmr::memory_resource &other) const noexcept override;
 
+    inline void set_fit_mode(allocator_with_fit_mode::fit_mode mode) override;
 
     std::vector<allocator_test_utils::block_info> get_blocks_info() const noexcept override;
 
-private:
+    unsigned char get_space_size_power() const;
 
-    
+    void *find_suitable_block(size_t required_k, allocator_with_fit_mode::fit_mode mode) const;
+
+private:
     inline logger *get_logger() const override;
-    
+
     inline std::string get_typename() const override;
 
     std::vector<allocator_test_utils::block_info> get_blocks_info_inner() const override;
 
     /** TODO: Highly recommended for helper functions to return references */
 
-    class buddy_iterator
-    {
-        void* _block;
+    class buddy_iterator {
+        void *_block;
 
     public:
-
         using iterator_category = std::forward_iterator_tag;
-        using value_type = void*;
-        using reference = void*&;
-        using pointer = void**;
+        using value_type = void *;
+        using reference = void *&;
+        using pointer = void **;
         using difference_type = ptrdiff_t;
 
-        bool operator==(const buddy_iterator&) const noexcept;
+        bool operator==(const buddy_iterator &) const noexcept;
 
-        bool operator!=(const buddy_iterator&) const noexcept;
+        bool operator!=(const buddy_iterator &) const noexcept;
 
-        buddy_iterator& operator++() & noexcept;
+        buddy_iterator &operator++() & noexcept;
 
         buddy_iterator operator++(int n);
 
@@ -135,11 +124,11 @@ private:
 
         bool occupied() const noexcept;
 
-        void* operator*() const noexcept;
+        void *operator*() const noexcept;
 
         buddy_iterator();
 
-        buddy_iterator(void* start);
+        buddy_iterator(void *start);
     };
 
     friend class buddy_iterator;
@@ -147,7 +136,6 @@ private:
     buddy_iterator begin() const noexcept;
 
     buddy_iterator end() const noexcept;
-    
 };
 
-#endif //MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_BUDDIES_SYSTEM_H
+#endif//MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_ALLOCATOR_BUDDIES_SYSTEM_H
